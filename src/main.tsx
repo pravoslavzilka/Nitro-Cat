30a59c65b85ca40618b79a76c3fcbc681c646dc1
Pravoslav <pravoslav.zilka@gmail.com>
2026-04-12 11:01:01 +0200

    Tracking clarity script injected

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/globals.css";
+// main.jsx
+import { clarity } from 'clarity-js';
+import { StrictMode } from 'react';
+
+
+clarity.start({
+  projectId: 'wa64f9yvoq',
+  upload: 'https://m.clarity.ms/collect',
+  track: true,
+  content: true,
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 createRoot(document.getElementById("root")!).render(<App />);
